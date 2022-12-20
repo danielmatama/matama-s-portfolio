541ffaec617210d1042cd81c36748be9ce4c37b4
daniel matama <danmatama2020@gmail.com>
2022-12-20 12:05:59 +0300

    Added more function to button

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const dropdownButton = document.querySelector(".dropdown-button");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 const mark = document.querySelector(".xmark");
+const getBody = document.body;
+const Items = document.querySelectorAll(".nav-bar .ax");
 dropdownButton.addEventListener("click", () => {
   dropdownMenu.classList.toggle("hide");
 });
 mark.addEventListener("click", () => {
   dropdownMenu.classList.toggle("hide");
+});
+Items.forEach((n) => {
+  n.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('hide');
+  });
 });
