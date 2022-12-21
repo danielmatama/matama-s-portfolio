a462d5479a79af16c91337fcc8781c2bee06f0b6
daniel matama <danmatama2020@gmail.com>
2022-12-21 17:53:53 +0300

    Improved desktop popup

--- script.js
+++ script.js
@@ -48,28 +48,47 @@ section.innerHTML = `
 
 
   <div class="popupdesk pophidedesk">
-
+  <div class = pheaddesk>
+  <span class="xsigndesk"><img src="images/desk.svg" alt=""></span>
   <img src="images/desktop.svg" alt="" class="popdesk-img">
-  <h2 class="phead">Keeping track of hundreds of
+  </div>
+  <div class="textdesk">
+  <h2 class="pheaddesk">Keeping track of hundreds of
     components</h2>
+    <ul class="pbuttons">
+    <button class="see" type="button" name="button">
+    <span>See Live</span>
+    <span><img src="images/upload.svg" alt=""></span>
+    </button>
+    <button class="see" type="button" name="button">
+    <span>See Source</span>
+    <span><img src="images/vector.svg" alt=""></span>
+    </button>
+    </ul>
+    </div>
     <ul class="plist">
-      <li><a href="#" class="panc card-a4">Ruby on rails</a></li>
-      <li><a href="#" class="panc card-a2">CSS</a></li>
+      <li><a href="#" class="panc card-a4">Codekit</a></li>
+      <li><a href="#" class="panc card-a2">Github</a></li>
       <li><a href="#" class="panc card-a3">Javscript</a></li>
+      <li><a href="#" class="panc card-a3">Bootstrap</a></li>
+      <li><a href="#" class="panc card-a3">Terminal</a></li>
     </ul>
   <p class="ptext">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <ul class="pbuttons">
-  <button class="see" type="button" name="button">
-  <span>See Live</span>
-  <span><img src="images/upload.svg" alt=""></span>
-  </button>
-  <button class="see" type="button" name="button">
-  <span>See Source</span>
-  <span><img src="images/vector.svg" alt=""></span>
-  </button>
-  </ul>
+
   </div>
 `;
+
+const projectDetails = [
+  {
+    name: 'Multi-post stories',
+    description: 'This is a dicription of my works.',
+    featuredImage: './img/coding-modified.png',
+    technologies: ['html', 'bootstrap', 'ruby'],
+    liveLink: 'See Live',
+    sourceLink: 'See Source',
+  },
+];
+
 
 const button = document.querySelectorAll(".see-button");
 const popup = document.querySelector(".popup");
@@ -94,6 +113,10 @@ window.addEventListener("resize", () => {
   if (window.innerWidth >=768) {
     popupdesk.classList.toggle("pophidedesk");
   }
+});
+const signdesk = document.querySelector(".xsigndesk");
+signdesk.addEventListener("click", () => {
+  popupdesk.classList.toggle("pophidedesk");
 });
 
 
