be12f37ead65ed9c184e69b5600f308f80a61168
daniel matama <danmatama2020@gmail.com>
2023-08-23 18:29:14 +0300

    make required changes

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ section.innerHTML = `
     </ul>
   <p class="ptext"> </p>
   <ul class="pbuttons">
-  <button class="see" type="button" name="button">
+  <button class="see" type="button" name="button" id="live-btn">
   <span>See Live</span>
   <span><img src="images/upload.svg" alt=""></span>
   </button>
-  <button class="see" type="button" name="button">
+  <button class="see" type="button" name="button" id="git-btn">
   <span>See Source</span>
   <span><img src="images/vector.svg" alt=""></span>
   </button>
@@ -52,14 +52,16 @@ section.innerHTML = `
   </div>
 `;
 
+
 const projectDetails = [{
-    name: 'Recipes App',
-    description: 'Recipes App is a website that provides a wide variety of recipe for users. Users have access to add recipes, delete recipes, add food and deletes food. The app also allows user to know what is required in order to prepare a recipe',
-    featuredImage: 'images/budget-foods.jpg',
+    name: 'Maths App',
+    description: 'Maths App is a web application for maths lovers, where a user can do their favourite calculations, and see quotes from their beloved mathmaticians.',
+    featuredImage: 'images/budget_foods.jpg',
     technologies: ['html', 'bootstrap', 'ruby'],
-    liveLink: 'See Live',
-    sourceLink: 'https://github.com/danielmatama/Recipe-app-rails',
-    mobileSrc: 'images/budget-foods.jpg',
+    liveLink: 'https://fluffy-beijinho-b768b3.netlify.app/',
+    sourceLink: 'https://github.com/danielmatama/react-app.git',
+    mobileSrc: 'images/maths2.png',
+    id: '1',
   },
 
   {
@@ -69,7 +71,8 @@ const projectDetails = [{
     technologies: ['html', 'bootstrap', 'ruby'],
     liveLink: 'https://tashbudgetapp.onrender.com',
     sourceLink: 'https://github.com/danielmatama/budget-app-rails',
-    mobileSrc: 'images/budget-app.jpg',
+    mobileSrc: 'images/budget_app2.png',
+    id: '2',
   },
 
   {
@@ -79,7 +82,8 @@ const projectDetails = [{
     technologies: ['html', 'bootstrap', 'ruby'],
     liveLink: 'https://book-an-appointment-frontend.onrender.com',
     sourceLink: 'https://github.com/calebchris000/Book-An-Appointment-Frontend',
-    mobileSrc: 'images/car.jpg',
+    mobileSrc: 'images/book3.png',
+    id: '3',
   },
 
   {
@@ -87,8 +91,10 @@ const projectDetails = [{
     description: 'The Blog App is an example of a blog website built using Ruby on Rails and PostgreSQL.',
     featuredImage: './img/coding-modified.png',
     technologies: ['html', 'bootstrap', 'ruby'],
+    liveLink: 'https://github.com/danielmatama/Blog-app-rails',
     sourceLink: 'https://github.com/danielmatama/Blog-app-rails',
-    mobileSrc: 'images/blog.jpg',
+    mobileSrc: 'images/blog1.jpg',
+    id: '4',
   },
 
   {
@@ -96,9 +102,10 @@ const projectDetails = [{
     description: 'This app is a must have instrument for book lovers. It is a webpage that allows you to add your favourite books and authors and save them. It also allows you to remove any books you may have already read or wish to replace. It will accept all books and authors without fail. Built with HTML, CSS, and JS.',
     featuredImage: './img/coding-modified.png',
     technologies: ['html', 'bootstrap', 'ruby'],
-    liveLink: 'See Live',
+    liveLink: 'https://github.com/danielmatama/bookstore-app-react',
     sourceLink: 'https://github.com/danielmatama/bookstore-app-react',
-    mobileSrc: 'images/bookstore.jpg',
+    mobileSrc: 'images/book_store2.png',
+    id: '5',
   },
 
   {
@@ -108,54 +115,11 @@ const projectDetails = [{
     technologies: ['html', 'bootstrap', 'ruby'],
     liveLink: 'https://metrics-matama.netlify.app',
     sourceLink: 'https://github.com/danielmatama/metrics-app',
-    mobileSrc: 'images/metrics.jpg',
+    mobileSrc: 'images/metrics_c2.png',
+    id: '6',
   },
 ];
 
-// ... buttons code
-
-// Loop through the project details and create the project cards
-projectDetails.forEach(project => {
-  const card = document.createElement('div');
-  card.classList.add('project-card');
-  card.innerHTML = `
-    <!-- ... card content ... -->
-    <div class="buttons">
-      <button class="see see-live" data-link="${project.liveLink}">
-        <span>See Live</span>
-        <span><img src="images/upload.svg" alt=""></span>
-      </button>
-      <button class="see see-source" data-link="${project.sourceLink}">
-        <span>See Source</span>
-        <span><img src="images/vector.svg" alt=""></span>
-      </button>
-    </div>
-  `;
-
-  section.appendChild(card);
-});
-
-// Add event listeners to the buttons
-const seeLiveButtons = document.querySelectorAll('.see-live');
-const seeSourceButtons = document.querySelectorAll('.see-source');
-
-seeLiveButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const liveLink = button.getAttribute('data-link');
-    if (liveLink) {
-      window.open(liveLink, '_blank'); // Open link in a new tab
-    }
-  });
-});
-
-seeSourceButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const sourceLink = button.getAttribute('data-link');
-    if (sourceLink) {
-      window.open(sourceLink, '_blank'); // Open link in a new tab
-    }
-  });
-});
 
 const button = document.querySelectorAll('.see-button');
 const popup = document.querySelector('.popup');
@@ -163,17 +127,37 @@ const popupWindow = document.querySelector('.popup-window')
 const sign = document.querySelector('.xsign');
 const windowHeader = document.querySelector('.phead-h2');
 const windowText = document.querySelector('.ptext');
-const windowImage = document.querySelector('.pop-img')
+const windowImage = document.querySelector('.pop-img');
+const liveBtn = document.getElementById('live-btn');
+const gitBtn = document.getElementById('git-btn');
 
 for (let i = 0; i < button.length; i += 1) {
   button[i].addEventListener('click', () => {
     popup.classList.toggle('pophide');
     popupWindow.classList.toggle('pophide');
+    popupWindow.id = `project_${projectDetails[i].id}`;
     windowHeader.innerHTML = projectDetails[i].name;
     windowText.innerHTML = projectDetails[i].description;
     windowImage.src = projectDetails[i].mobileSrc;
-  })
+
+  });
+
 }
+liveBtn.addEventListener('click', ()=>{
+    projectDetails.forEach(project => {
+      if (!!liveBtn.closest(`#project_${project.id}`)) {
+        window.location.href = project.liveLink;
+      }
+    })
+});
+
+gitBtn.addEventListener('click', ()=>{
+  projectDetails.forEach(project => {
+    if (!!gitBtn.closest(`#project_${project.id}`)) {
+      window.location.href = project.sourceLink;
+    }
+  })
+});
 
 sign.addEventListener('click', () => {
   popup.classList.toggle('pophide');
@@ -230,12 +214,4 @@ valueInput.forEach(showNow);
 form.addEventListener('change', (event) => {
   event.preventDefault();
   addVale(namee.value, email.value, text.value);
-});
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > header.offsetHeight) {
-    navBar.classList.add('sticky-nav');
-  } else {
-    navBar.classList.remove('sticky-nav');
-  }
 });
